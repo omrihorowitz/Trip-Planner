b4aeceae1ba68efaffb4bcfc9a67e80e9263b744
Chris <withscottc921@gmail.com>
2021-03-08 20:05:42 -0700

    Initial Commit

---
+++ TripPlanner/Controllers/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripPlanner
+//
+//  Created by Chris Withers on 3/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
