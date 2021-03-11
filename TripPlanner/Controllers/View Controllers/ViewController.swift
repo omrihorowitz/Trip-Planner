8c8df50a123a7a80dc2e535b3c26614d9e573806
Omri Horowitz <horowitzomri@gmail.com>
2021-03-11 12:59:08 -0800

    pin to reverse geolocate

--- TripPlanner/Controllers/View Controllers/ViewController.swift
+++ TripPlanner/Controllers/View Controllers/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
+    var previousLocation: CLLocation?
     var mapView = MKMapView()
+    let pin = UIImageView(image: #imageLiteral(resourceName: "icons8-map-pin-50-3"))
+    let addressLabel = UILabel()
+    
     private var landmarks: [Landmark] = []
     
     override func viewDidLoad() {
@@ -33,32 +37,39 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func allConfiguration() {
         configureMap()
+        configurePin()
+        configureAddressLabel()
     }
     
     func addSubViews() {
         view.addSubview(mapView)
+        view.addSubview(pin)
+        view.addSubview(addressLabel)
     }
     
     func configureMap() {
         // Set initial location in Santa Monica
-//        let initialLocation = CLLocation(latitude: 34.0195, longitude: -118.4912)
-//        mapView.centerToLocation(initialLocation)
-//        let smCenter = CLLocation(latitude: 34.0099, longitude: -118.4960)
-//        let region = MKCoordinateRegion(
-//            center: smCenter.coordinate,
-//            latitudinalMeters: 5000, //how constrained can users look in region
-//            longitudinalMeters: 6000)
-//        mapView.setCameraBoundary(
-//            MKMapView.CameraBoundary(coordinateRegion: region),
-//            animated: true)
-
-//        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
-//        mapView.setCameraZoomRange(zoomRange, animated: true)
-        let leftMargin:CGFloat = 0
-        let topMargin:CGFloat = 0
-        let mapWidth:CGFloat = view.frame.size.width
-        let mapHeight:CGFloat = view.frame.size.height
-        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+        //        let initialLocation = CLLocation(latitude: 34.0195, longitude: -118.4912)
+        //        mapView.centerToLocation(initialLocation)
+        //        let smCenter = CLLocation(latitude: 34.0099, longitude: -118.4960)
+        //        let region = MKCoordinateRegion(
+        //            center: smCenter.coordinate,
+        //            latitudinalMeters: 5000, //how constrained can users look in region
+        //            longitudinalMeters: 6000)
+        // camera boundary by city
+        //        mapView.setCameraBoundary(
+        //            MKMapView.CameraBoundary(coordinateRegion: region),
+        //            animated: true)
+        // zoom range by city
+        //        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+        //        mapView.setCameraZoomRange(zoomRange, animated: true)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
         mapView.mapType = MKMapType.mutedStandard //diff map types
         mapView.isZoomEnabled = true
         mapView.isScrollEnabled = true
@@ -70,6 +81,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         loadInitialData()
         mapView.addAnnotations(landmarks)
     }
+    
+    func configurePin() {
+        pin.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pin.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
+            pin.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pin.widthAnchor.constraint(equalToConstant: 40),
+            pin.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    func configureAddressLabel() {
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        addressLabel.backgroundColor = .white
+        addressLabel.textColor = .black
+        addressLabel.font = UIFont(name: "AvenirNext-DemiBold", size: 20)
+        addressLabel.textAlignment = .center
+        NSLayoutConstraint.activate([
+            addressLabel.heightAnchor.constraint(equalToConstant: 50),
+            addressLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            addressLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            addressLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
     
     private func loadInitialData() {
         // 1
@@ -119,35 +155,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func checkLocationAuthorization() {
         switch locationManager.authorizationStatus {
         case .authorizedWhenInUse:
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
+            startTrackingUserLocation()
+        case .denied:
+            // show alert instructing them to turn on permissions
             break
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
         case .restricted:
             // show an alert letting them know what's up
             break
-        case .denied:
-            // show alert instructing them to turn on permissions
-            break
         case .authorizedAlways:
-            break
+            startTrackingUserLocation()
         @unknown default:
             break
         }
-        
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
     }
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         checkLocationAuthorization()
+    }
+    
+    func startTrackingUserLocation() {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitutde = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitutde)
     }
 }
 
@@ -158,7 +197,7 @@ private extension MKMapView {
     ) {
         let coordinateRegion = MKCoordinateRegion(
             center: location.coordinate,
-            latitudinalMeters: regionRadius, //defauly 1k meters
+            latitudinalMeters: regionRadius, //default 1k meters
             longitudinalMeters: regionRadius)
         setRegion(coordinateRegion, animated: true)
     }
@@ -178,5 +217,35 @@ extension ViewController: MKMapViewDelegate {
             MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
         ]
         landmark.mapItem?.openInMaps(launchOptions: launchOptions)
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else { return }
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            
+            if let _ = error {
+                //todo: show alert informing user
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                //todo: show alert informing user
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName)"
+            }
+        }
     }
 }
